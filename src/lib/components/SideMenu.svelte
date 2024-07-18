a7b7ac06f16356ef17755c87cb8e3016d7d24493
DavidYong <yongcheeho@gmail.com>
2024-07-18 14:18:37 +0800

    feat: side menu with path

---
+++ src/lib/components/SideMenu.svelte
@@ -0,0 +1,19 @@
+<script>
+    const menuList = [
+        {name: "Home", path: '/'},
+        {name: "Game #1", path: '/game-1'},
+        {name: "Stake", path: '/stake'},
+        {name: "Mission", path: '/mission'},
+        {name: "Leaderboard", path: '/leaderboard'}
+    ]
+</script>
+
+<div class="bg-gray-500 min-h-screen flex flex-col p-5 gap-5 min-w-[200px]">
+    {#each menuList as menu}
+        <div>
+            <a href={menu.path} class="hover:text-gray-300">
+                {menu.name}
+            </a>
+        </div>
+    {/each}
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import Nprogress from '$lib/components/ui/nprogress/nprogress.svelte';
 	import { Toaster } from '$lib/components/ui/toast';
+	import SideMenu from '$lib/components/SideMenu.svelte';
 	import '../app.postcss';
 </script>
 
 <div class="w-full h-full min-h-screen relative z-10 transition">
 	<Nprogress />
-	<slot />
+	<div class='flex'>
+		<SideMenu />
+		<slot />
+	</div>
+
 	<Toaster />
 </div>
