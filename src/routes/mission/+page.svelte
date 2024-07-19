23f58f9881dcac0336466526b3308fe00f018c28
DavidYong <yongcheeho@gmail.com>
2024-07-19 15:22:08 +0800

    feat: mission page desktop view

--- src/routes/mission/+page.svelte
+++ src/routes/mission/+page.svelte
@@ -3,9 +3,10 @@
 	import { Button } from "$lib/components/ui/button";
 	import * as Card from '$lib/components/ui/card';
     import Text from "$lib/components/ui/text/text.svelte";
+    import Separator from "$lib/components/ui/separator/separator.svelte";
 
     const statusList = [
-        {name:"Your Points", value:'100,000 points'},
+        {name:"Your Points", value:'1000'},
         {name:"Completed Mission", value:'10'},
         {name:"Ongoing Mission", value:'8'}
     ]
@@ -14,15 +15,16 @@
 
 </script>
 <div class="h-full w-full min-h-screen p-3 xl:p-5 space-y-10">
-	<div id="Mission" class="space-y-5 flex flex-col">
+	<div id="Mission" class="space-y-5 flex flex-col w-full">
 		<div class="flex justify-between w-full">
 			<Text tag="h1" size="_4xl">Mission</Text>
 			<Button class=""><Text tag="p" size="lg" class="text-white">Bind Referral</Text></Button>
 		</div>
 
-        <div id="Status" class="flex flex-col">
+        <!-- mobile view of status-->
+        <div id="Status" class="flex flex-col xl:hidden ">
             <Text tag='h4' size='lg' class='font-semibold'>Your Status</Text>
-            <Card.Root class="w-full xl:w-1/4 flex flex-col justify-center p-3">
+            <Card.Root class="w-full xl:w-1/4 flex flex-col justify-center p-3 rounded-xl">
                 {#each statusList as status}
                     <div class="flex justify-between">
                         <Text>{status.name}:</Text>
@@ -33,11 +35,32 @@
             </Card.Root>
         </div>
 
-        <div id="Mission List" class="flex flex-col">
-            <Text tag='h1' size='_3xl' class='font-bold'>Mission List</Text>
-            <Card.Root class="w-full xl:w-1/4 flex flex-col justify-center p-3 gap-3">
+        <!-- desktop view of status-->
+        <div class="flex justify-center hidden xl:flex">
+            <div class="grid grid-cols-5 h-[100px] w-[80%]">
+                {#each statusList as status, i}
+                <div class="flex flex-col items-center gap-3">
+                    <Text tag='h1' size='_2xl' class='whitespace-nowrap overflow-hidden'>{status.name}:</Text>
+                    
+                    <Text tag='h1' size='_4xl' class='font-bold text-[35px]'>{status.value}</Text>
+                </div>
+    
+                <div class="flex justify-center h-full">
+                    {#if i !== statusList.length - 1}
+                    <Separator orientation='vertical' class='h-full bg-gray-300 max-w-[1px]' />
+                    {/if}
+                </div>
+    
+                {/each}
+            </div>
+        </div>
+      
+
+        <div id="Mission List" class="flex flex-col w-full">
+            <Text tag='h1' size='_4xl' class='font-bold '>Mission List</Text>
+            <Card.Root class="w-full flex flex-col justify-center p-3 gap-3 rounded-xl">
                 {#each sampleMissionData as mission}
-                    <Card.Root class="flex justify-between p-2 items-center">
+                    <Card.Root class="flex justify-between p-2 items-center rounded-sm">
                         <Text>{mission}</Text>
                         <Button size='sm' class='px-5 '>Start</Button>
                     </Card.Root>
