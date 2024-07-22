a60bfa6cf057ccbc0b4ad74d1a0595ab907473b9
cl3ment_ch <chienher@hotmail.com>
2024-07-22 16:47:15 +0800

    feat: :sparkles: update user info from layout

---
+++ src/routes/referral/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import UserAPI from '$lib/api/user';
+	import { onMount } from 'svelte';
+	import { storeUserInfo } from '$lib/stores/storeUser';
+
+	onMount(async () => {
+	});
+</script>
