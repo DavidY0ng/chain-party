274903517d134cd044d5a7837fbc400bdbe492af
cl3ment_ch <chienher@hotmail.com>
2024-07-23 19:22:35 +0800

    feat: :sparkles: added login features to mobile drawer

--- src/lib/components/shared/ConnectWallet.svelte
+++ src/lib/components/shared/ConnectWallet.svelte
@@ -2,33 +2,45 @@
 	import { goto } from '$app/navigation';
 	import AuthAPI from '$lib/api/auth';
 	import { Button, type Props } from '$lib/components/ui/button';
+	import { isDesktop } from '$lib/stores/storeCommon';
 	import { storeUserInfo } from '$lib/stores/storeUser';
 	import { cn, getUserProfile } from '$lib/utils';
 	import { account, connectWallet, onDisconnect } from '$lib/web3/wagmi';
 	import { toast } from 'svelte-sonner';
 	import { zeroAddress } from 'viem';
 
-	type $$Props = Props;
+	type $$Props = Props & { drawerOpen: boolean };
 
 	let className: $$Props['class'] = undefined;
 	export { className as class };
+	export let drawerOpen: boolean = false;
 
 	const onConnectWallet = async () => {
 		await connectWallet();
 		const auth = await AuthAPI.requestMessage($account.address!);
 		await getUserProfile();
-		if (auth) toast.success('Connected Wallet');
+		if (auth) {
+			toast.success('Connected Wallet');
+		} else {
+			toast.error('Failed to login');
+		}
+		if (!$isDesktop) drawerOpen = false;
 	};
 
 	const onHandleDisconnect = async () => {
 		await AuthAPI.logout();
 		await onDisconnect();
+		if (!$isDesktop) drawerOpen = false;
 		goto('/');
 	};
 </script>
 
 {#if $storeUserInfo.web3_address === zeroAddress}
-	<Button class={cn('', className)} on:click={onConnectWallet}>Connect Wallet</Button>
+	<Button class={cn('text-lg xl:text-md', className)} on:click={onConnectWallet}>
+		Connect Wallet
+	</Button>
 {:else}
-	<Button class={cn('', className)} on:click={onHandleDisconnect}>Disconnect</Button>
+	<Button class={cn('text-lg xl:text-md', className)} on:click={onHandleDisconnect}>
+		Disconnect
+	</Button>
 {/if}
