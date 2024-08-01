ed241bd0f8125b816df7620d3128e7b8b2e7d568
DavidYong <yongcheeho@gmail.com>
2024-08-01 15:32:48 +0800

    chores: turn off coming soon

--- src/lib/stores/storeCommon.ts
+++ src/lib/stores/storeCommon.ts
@@ -7,7 +7,7 @@ import { storeUserInfo } from './storeUser';
 export const isDesktop = mediaQuery('(min-width: 1280px)');
 export const isToken = writable<string | undefined>(undefined);
 export const rerender = writable<boolean>(false);
-export const isComingSoon = writable<boolean>(true);
+export const isComingSoon = writable<boolean>(false);
 
 // Subscribe to storeUserInfo changes
 storeUserInfo.subscribe((status) => {
