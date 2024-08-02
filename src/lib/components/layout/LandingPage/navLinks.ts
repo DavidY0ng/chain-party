1c7351e8f00cc5aa4844ba98ecf7d8424cb18d52
DavidYong <yongcheeho@gmail.com>
2024-08-02 12:44:38 +0800

    chores: added landing nav links translation

--- src/lib/components/layout/LandingPage/navLinks.ts
+++ src/lib/components/layout/LandingPage/navLinks.ts
@@ -1,5 +1,7 @@
+import { t } from '$lib/i18n'
+
 export const navLinks = [
-    { name: 'Home', path: '/' },
-    { name: 'Mechanism', path: 'https://docs.chainparty.xyz/chain-party/what-is-chain-party' },
-    { name: 'Game', path: '/home' }
+    { name: t.get('landing.nav_links.home'), path: '/' },
+    { name: t.get('landing.nav_links.mechanism'), path: 'https://docs.chainparty.xyz/chain-party/what-is-chain-party' },
+    { name: t.get('landing.nav_links.game'), path: '/home' }
 ];
