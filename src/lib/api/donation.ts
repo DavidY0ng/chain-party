1f65bd6ebb73c8e5e9d8e55c75e9da13db51cb76
cl3ment_ch <chienher@hotmail.com>
2024-08-05 12:34:47 +0800

    feat: :sparkles: added donation api

---
+++ src/lib/api/donation.ts
@@ -0,0 +1,29 @@
+import { api, type APIResponse } from '$lib/http/https';
+
+type TDonationLeaderboard = {
+	data: {
+		rank: number;
+		address: string;
+		amount: string;
+		is_self: boolean;
+	}[];
+	count: number;
+};
+
+const DonationAPI = {
+	getLeaderboard: async function (size: number): Promise<APIResponse<TDonationLeaderboard>> {
+		try {
+			const response = await api.get<TDonationLeaderboard>('/dapp/donation/leaderboard', {
+				data: {
+					size
+				}
+			});
+			return response;
+		} catch (error) {
+			return { success: false, data: {} as TDonationLeaderboard, msg: (error as Error).message };
+		}
+	}
+};
+
+export default DonationAPI;
+export type { TDonationLeaderboard };
--- src/routes/(app)/donate/+page.svelte
+++ src/routes/(app)/donate/+page.svelte
@@ -3,30 +3,48 @@
 	import { Text } from '$lib/components/ui/text';
 	import Icon from '@iconify/svelte';
 	import * as Table from '$lib/components/ui/table';
+	import DonationAPI, { type TDonationLeaderboard } from '$lib/api/donation';
+	import { onMount } from 'svelte';
+
+	let leaderboardSize: number = 53;
+	let leaderboardData: TDonationLeaderboard;
+
+	async function onGetLeaderBoard() {
+		const result = await DonationAPI.getLeaderboard(leaderboardSize);
+		if (result.success) {
+			leaderboardData = result.data;
+		} else {
+			throw new Error('Failed to fetch leaderboard');
+		}
+	}
+
+	onMount(() => {
+		onGetLeaderBoard();
+	});
 </script>
 
-<div class="h-full w-full min-h-screen space-y-5 xl:space-y-10">
-	<div class="grid grid-cols-2 justify-items-center mx-auto xl:w-[80%] gap-5">
+<div class="h-full min-h-screen w-full space-y-5 xl:space-y-10">
+	<div class="mx-auto grid grid-cols-2 justify-items-center gap-5 xl:w-[80%]">
 		{#each Array(3) as _, i}
 			<Card.Root
-				class="xl:p-5 flex xl:flex-row flex-col items-center gap-x-5 border-none xl:bg-gradient-to-l from-black/10 {i ===
+				class="flex flex-col items-center gap-x-5 border-none from-black/10 xl:flex-row xl:bg-gradient-to-l xl:p-5 {i ===
 				0
 					? 'col-span-full w-1/2 '
 					: 'w-full'}"
 			>
-				<div class="border rounded-full p-5 xl:p-0 xl:border-none xl:rounded-none">
+				<div class="rounded-full border p-5 xl:rounded-none xl:border-none xl:p-0">
 					<Icon icon="emojione:sports-medal" class="text-[70px] xl:text-[120px]" />
 				</div>
 				<Text size="xl" class="block xl:hidden">Address {i + 1}</Text>
 
-				<div class="w-full relative space-y-2 hidden xl:block">
+				<div class="relative hidden w-full space-y-2 xl:block">
 					<Text size="3xl">Dummy {i + 1}</Text>
-					<div class="w-full flex items-center">
+					<div class="flex w-full items-center">
 						<div class="w-full">
 							<Text size="2xl" class="text-black/50">Token Holding</Text>
 							<Text size="2xl" class="text-black/50">10,000</Text>
 						</div>
-						<hr class="h-[40px] w-[1px] bg-black/50 mr-10" />
+						<hr class="mr-10 h-[40px] w-[1px] bg-black/50" />
 						<div class="w-full">
 							<Text size="2xl" class="text-black/50">Token Holding</Text>
 							<Text size="2xl" class="text-black/50">10,000</Text>
@@ -40,7 +58,7 @@
 	<!-- Desktop Leaderboard list -->
 	<Table.Root class="hidden xl:table">
 		<Table.Header>
-			<Table.Row class="hover:bg-transparent text-lg border-none w-full">
+			<Table.Row class="w-full border-none text-lg hover:bg-transparent">
 				<Table.Head class="">Rank</Table.Head>
 				<Table.Head class="">Username</Table.Head>
 				<Table.Head class="">Token Holding</Table.Head>
@@ -49,7 +67,7 @@
 		</Table.Header>
 		<Table.Body class="">
 			{#each Array(3) as _, i}
-				<Table.Row class="text-lg border-none bg-gradient-to-l from-black/10 ">
+				<Table.Row class="border-none bg-gradient-to-l from-black/10 text-lg ">
 					<Table.Cell
 						class="font-medium text-black {i === 0
 							? ' rounded-tl-lg'
@@ -74,14 +92,14 @@
 	<!-- Mobile Leaderboard list -->
 	<div id="Mobile Leaderboard List" class="space-y-3 xl:hidden">
 		<Text size="xl" class="text-black">Leaderboard List</Text>
-		<Card.Root class="flex justify-between items-center px-2 py-3">
-			<Text size="lg" class="text-black font-semibold">My Address</Text>
-			<Text size="lg" class="text-black font-semibold">10,000 EIC</Text>
+		<Card.Root class="flex items-center justify-between px-2 py-3">
+			<Text size="lg" class="font-semibold text-black">My Address</Text>
+			<Text size="lg" class="font-semibold text-black">10,000 EIC</Text>
 		</Card.Root>
 		{#each Array(3) as _, i}
-			<Card.Root class="flex justify-between items-center px-2 py-3">
-				<Text size="lg" class="text-black font-semibold">Player {i + 1}</Text>
-				<Text size="lg" class="text-black font-semibold">10,000 EIC</Text>
+			<Card.Root class="flex items-center justify-between px-2 py-3">
+				<Text size="lg" class="font-semibold text-black">Player {i + 1}</Text>
+				<Text size="lg" class="font-semibold text-black">10,000 EIC</Text>
 			</Card.Root>
 		{/each}
 	</div>
