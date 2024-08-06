5094785514add11b03d01023461d5446cb18c6eb
DavidYong <yongcheeho@gmail.com>
2024-08-06 14:49:52 +0800

    feat: added winner list for guest and logged in user

--- src/lib/components/page/jackpot/WinnerList.svelte
+++ src/lib/components/page/jackpot/WinnerList.svelte
@@ -1,12 +1,15 @@
 <script>
-    import { t } from '$lib/i18n';
-    import Text from '$lib/components/ui/text/text.svelte';
-    import { Button } from '$lib/components/ui/button';
-    import JackpotAPI from '$lib/api/jackpot';
+	import { t } from '$lib/i18n';
+	import Text from '$lib/components/ui/text/text.svelte';
+	import { Button } from '$lib/components/ui/button';
+	import JackpotAPI from '$lib/api/jackpot';
 	import { onMount } from 'svelte';
-    import Icon from '@iconify/svelte';
+	import Icon from '@iconify/svelte';
+	import { storeUserInfo } from '$lib/stores/storeUser';
+	import { isToken } from '$lib/stores/storeCommon';
+	import { zeroAddress } from 'viem';
 
-    let winnerListPagination = {
+	let winnerListPagination = {
 			page: 1,
 			size: 1
 		},
@@ -19,29 +22,49 @@
 		const result = await JackpotAPI.getWinnerList(winnerListPagination);
 	}
 
-    onMount(() => {
+	onMount(() => {
 		getWinnerList();
 	});
 </script>
 
 <div id="winner list">
-    <div class="w-full space-y-3">
-        <div
-            class="flex w-full items-center justify-between rounded-xl bg-[#481555] font-bold overflow-hidden"
-        >
-            <Text class='pl-8'>{$t('jackpot.winner_list')}</Text>
-            <div class="flex items-center gap-3 bg-gradient-to-t from-[#AC4FF4] to-[#F72E9A] py-2 px-4">
-                <Button class="underline font-thin p-0">{$t('jackpot.how_to_join')}</Button>
-                <Icon icon="fluent:open-24-filled" width="1.2em" height="1.2em" />
-            </div>
-        </div>
-        <div class="h-[500px] w-full overflow-y-scroll rounded-2xl bg-black/20">
-            {#each Array(10) as _, i}
-                <div class="flex items-center justify-between px-8 py-4">
-                    <Text>0x9693CD9713496b0712f52E5F0c7b8948abdA824D</Text>
-                    <Text>Won 813 pEIC</Text>
-                </div>
-            {/each}
-        </div>
-    </div>
+	<div class="w-full space-y-3">
+		<div
+			class="gradient-border-bottom flex w-full items-center justify-between overflow-hidden rounded-xl bg-[#481555] font-bold"
+		>
+			<Text class="pl-8">{$t('jackpot.winner_list')}</Text>
+			<div class="flex items-center gap-3 bg-gradient-to-t from-[#AC4FF4] to-[#F72E9A] px-4 py-2">
+				<Button class="p-0 font-thin underline">{$t('jackpot.how_to_join')}</Button>
+				<Icon icon="fluent:open-24-filled" width="1.2em" height="1.2em" />
+			</div>
+		</div>
+		<div class="h-[500px] w-full overflow-y-scroll rounded-2xl bg-black/20">
+			{#if $storeUserInfo.web3_address !== zeroAddress && $isToken !== undefined}
+				{#each Array(10) as _, i}
+					<div class="flex items-center justify-between px-8 py-4">
+						<Text>0x9693CD9713496b0712f52E5F0c7b8948abdA824D</Text>
+						<div class="flex justify-between gap-x-[100px]">
+							<div class="flex gap-x-2">
+								<img src="/img/jackpot/cap.png" class="h-[20px] w-[20px]" alt="cap" />
+								<Text class="">Won</Text>
+							</div>
+							<Text>813 pEIC</Text>
+						</div>
+					</div>
+				{/each}
+			{:else}
+				{#each Array(10) as _, i}
+					<div class="flex items-center justify-between px-8 py-4">
+						<Text>0x9693CD9713496b0712f52E5F0c7b8948abdA824D</Text>
+						<div class="flex">
+							<Text>WON 813 pEIC</Text>
+						</div>
+					</div>
+				{/each}
+			{/if}
+		</div>
+	</div>
 </div>
+
+<style>
+</style>
