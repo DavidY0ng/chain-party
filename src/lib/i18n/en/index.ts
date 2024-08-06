10d943c59213d690fbde3f27c361f072290bf0b5
DavidYong <yongcheeho@gmail.com>
2024-08-06 10:12:11 +0800

    chores: reorganize translation json

--- src/lib/i18n/en/index.ts
+++ src/lib/i18n/en/index.ts
@@ -18,7 +18,37 @@ export default [
 	},
 	{
 		locale: lang,
-		key: 'app',
-		loader: async () => (await import('./app.json')).default
+		key: 'donate',
+		loader: async () => (await import('./donate.json')).default
+	},
+	{
+		locale: lang,
+		key: 'game',
+		loader: async () => (await import('./game.json')).default
+	},
+	{
+		locale: lang,
+		key: 'history',
+		loader: async () => (await import('./history.json')).default
+	},
+	{
+		locale: lang,
+		key: 'home',
+		loader: async () => (await import('./home.json')).default
+	},
+	{
+		locale: lang,
+		key: 'jackpot',
+		loader: async () => (await import('./jackpot.json')).default
+	},
+	{
+		locale: lang,
+		key: 'referral',
+		loader: async () => (await import('./referral.json')).default
+	},
+	{
+		locale: lang,
+		key: 'stake',
+		loader: async () => (await import('./stake.json')).default
 	},
 ];
