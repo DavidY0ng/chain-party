fb9b3ba862be3a011bb4d14ed9a79b92907bba2b
DavidYong <yongcheeho@gmail.com>
2024-08-12 22:27:17 +0800

    chores: updated landing intro UI

--- src/lib/components/page/landing/Intro.svelte
+++ src/lib/components/page/landing/Intro.svelte
@@ -1,13 +1,28 @@
-<div class="flex flex-col items-center gap-5 w-full">
-    <div class="bg-gray-500 h-[400px] max-w-[600px] w-full">
-        <img src='' class='' alt='chain_party_mascot'>
-    </div>
 
-    <div class="bg-gray-500 h-[100px] max-w-[600px] w-full">
-        <img src='' alt="chain_party_logo">
-    </div>
+<div class="flex flex-col items-center justify-center gap-5 w-full bg-[url('/img/landing/intro/background.jpg')] bg-cover bg-no-repeat bg-center h-dvh">
 
-    <div class="bg-gray-500 h-[100px] max-w-[600px] w-full">
-        <img src='' alt="investor_logo">
+    <div class=" xl:h-[800px] xl:w-[700px]">
+        <div class="h-full xl:h-[760px] max-w-[700px] w-full relative flex flex-col items-center justify-center">
+            <img src='/img/landing/intro/chain party.png' class='xl:h-full xl:w-full' alt='chain_party_mascot'>
+            <div class=" w-full px-3 flex justify-center absolute xl:bottom-[5%] bottom-[5%]">
+                <!-- trapezium container -->
+                <img src="/img/landing/intro/trapezium.png" alt="trapezium" class="xl:w-[667px] xl:h-[173px]" />
+                <div
+                    class="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] capitalize text-white text-center w-[80%] px-4
+                            pixel-font md:text-[28px] text-[18px] font-bold md:leading-[38.168px] tracking-[0.05em] md:w-[70%]"
+                >
+                    The 1st Gamefi build on EIC Chain
+                </div>
+            </div>
+        </div>
+        <div class="container flex items-center justify-center mx-auto space-x-4">
+            <div class="uppercase font-inter text-xs font-normal leading-[14.52px] text-white">
+                Incubated By
+            </div>
+            <img src='/img/landing/logo/eic logo.png' alt='eic-logo' class="h-8"> <!-- Adjust height as needed -->
+        </div>
     </div>
+    
+
+    
 </div>
