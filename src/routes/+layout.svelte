93c689c7b998cd3e612f62655904b4ae872daf92
DavidYong <yongcheeho@gmail.com>
2024-08-14 11:37:25 +0800

    fix: redirect issue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { showBindReferral } from '$lib/stores/storeCommon';
-	import { page } from '$app/stores';
-	import { redirect } from '@sveltejs/kit';
-	import { browser } from '$app/environment';
 
-	$: if ( $page.url.pathname !== '/') {
-		throw redirect(307, '/');
-	}
 </script>
 
 <slot />
