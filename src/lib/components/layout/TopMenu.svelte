aa29431542c47fba29f003af631600e791e0c100
cl3ment_ch <chienher@hotmail.com>
2024-08-15 18:07:33 +0800

    feat: :sparkles: update latest connect wallet button

--- src/lib/components/layout/TopMenu.svelte
+++ src/lib/components/layout/TopMenu.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index';
 	import { Text } from '$lib/components/ui/text';
-	import { isDesktop } from '$lib/stores/storeCommon';
+	import { truncateString } from '$lib/helper';
+	import { isDesktop, isToken } from '$lib/stores/storeCommon';
+	import { storeUserInfo } from '$lib/stores/storeUser';
+	import Icon from '@iconify/svelte';
+	import { zeroAddress } from 'viem';
 	import ConnectWallet from '../shared/ConnectWallet.svelte';
-	import Drawer from './Drawer.svelte';
 	import MultiLanguage from '../shared/MultiLanguage.svelte';
+	import { Button } from '../ui/button';
+	import Drawer from './Drawer.svelte';
+	import { onDisconnect } from '$lib/web3/wagmi';
+	import AuthAPI from '$lib/api/auth';
+
+	let open = false;
+
+	const onHandleDisconnect = async () => {
+		await AuthAPI.logout();
+		await onDisconnect();
+	};
 </script>
 
 <div
@@ -18,10 +33,51 @@
 			<MultiLanguage />
 			<Drawer />
 		</div>
-		
+
 		<!-- Mobile Drawer -->
 		{#if $isDesktop}
-			<ConnectWallet class="hidden xl:block" />
+			{#if $storeUserInfo.web3_address === zeroAddress || $isToken === undefined}
+				<ConnectWallet class="hidden xl:block" />
+			{:else}
+				<DropdownMenu.Root bind:open>
+					<DropdownMenu.Trigger asChild let:builder>
+						<Button
+							builders={[builder]}
+							variant="outline"
+							class="overflow-hidden rounded-lg border-none bg-black/50 p-0 hover:bg-black/80"
+						>
+							<div class="flex h-full w-[60px] items-center justify-center bg-[#FF0099]">
+								<img src="/img/desktopNav/wallet.png" alt="" class="-mt-1 w-[18px]" />
+							</div>
+							<div class="flex w-full items-center gap-x-2 pl-5">
+								{truncateString($storeUserInfo.web3_address, 4, 4)}
+								<Icon
+									icon="tabler:chevron-down"
+									class={open ? 'rotate-180 transition' : 'rotate-0 transition'}
+								/>
+							</div>
+						</Button>
+					</DropdownMenu.Trigger>
+					<DropdownMenu.Content class="w-56 border-none bg-[#251235]">
+						<DropdownMenu.Item
+							on:click={onHandleDisconnect}
+							class="wallet-dropdown flex cursor-pointer gap-x-2"
+						>
+							<Icon icon="material-symbols:logout" class="text-2xl" />
+							Disconnect</DropdownMenu.Item
+						>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
+			{/if}
 		{/if}
 	</div>
 </div>
+
+<style>
+	:global(.wallet-dropdown) {
+		transition: background-color 0.2s ease;
+	}
+	:global(.wallet-dropdown:hover) {
+		background-color: #3a1b50 !important;
+	}
+</style>
