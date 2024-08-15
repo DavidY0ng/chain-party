53fe2a99fb9074a532a3825def5c486429d0a302
DavidYong <yongcheeho@gmail.com>
2024-08-15 18:50:30 +0800

    chores: remove contract from modal

--- src/lib/components/page/donate/DonationModal.svelte
+++ src/lib/components/page/donate/DonationModal.svelte
@@ -7,9 +7,7 @@
 	import { Checkbox } from '$lib/components/ui/checkbox';
 	import { Label } from '$lib/components/ui/label';
 	import { t } from '$lib/i18n';
-	import { gameContract } from '$lib/web3/contract/contract';
 	import { storeUserInfo } from '$lib/stores/storeUser';
-	import { bscChain } from '$lib/web3/client';
 
 	let showModal = false;
 	let donationAmount: string | undefined = undefined;
@@ -21,7 +19,7 @@
 
 	$: userAddress = $storeUserInfo.web3_address
 
-	async function onDonate(amount:number) {
+	async function onDonate() {
 		if (+donationAmount! < 1) {
 			donationError = 'Amount must be more than 1';
 			return;
@@ -109,7 +107,7 @@
 					type="button"
 					class="w-full text-md"
 					disabled={!donationAmount || +donationAmount < 1 || !isChecked.value}
-					on:click={() => onDonate(donationAmount)}>Donate</Button
+					on:click={() => onDonate}>Donate</Button
 				>
 				</div>
 				
