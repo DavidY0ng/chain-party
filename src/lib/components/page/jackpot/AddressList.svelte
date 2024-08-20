fafa2673166aa6c178bd3fbb865678739357c9c8
DavidYong <yongcheeho@gmail.com>
2024-08-20 17:16:53 +0800

    chores: updated current list UI

--- src/lib/components/page/jackpot/AddressList.svelte
+++ src/lib/components/page/jackpot/AddressList.svelte
@@ -53,18 +53,20 @@
 			<Text>{$t('home.address')}</Text>
 			<Text>{$t('home.won_times')}</Text>
 		</div>
-		{#if currentList?.data.length > 0}
-			{#each currentList.data as user, i}
-				{#if user.is_self}
-					<div class="overflow-hidden rounded-2xl">
-						<div class="selfContainer w-full border-x-[6px]">
-							<div class="rounded bg-[#000000]/30 px-6 py-4">
-								<Text>{user.address}</Text>
-							</div>
-						</div>
+		{#if currentList?.self}
+			
+				
+			<div class="overflow-hidden rounded-2xl">
+				<div class="selfContainer w-full border-x-[6px]">
+					<div class="rounded bg-[#000000]/30 px-6 py-4 flex justify-between">
+						<Text class="flex md:hidden">{truncateString(currentList.self.address,10,10)}</Text>
+						<Text class="hidden md:flex">{currentList.self.address}</Text>
+						<Text class='pr-3'>{currentList.self.count}</Text>
 					</div>
-				{/if}
-			{/each}
+				</div>
+			</div>
+			
+			
 		{/if}
 		<div
 			class="gradientScrollbar {$storeUserInfo.web3_address === zeroAddress &&
@@ -75,7 +77,8 @@
 			{#if currentList?.data.length > 0}
 				{#each currentList?.data as user, i}
 					<div class="flex items-center justify-between px-8 py-4">
-						<Text>{truncateString(user?.address, 7, 7)}</Text>
+						<Text class="flex md:hidden">{truncateString(user?.address, 10, 10)}</Text>
+						<Text class="hidden md:flex">{user?.address}</Text>
 						<Text>{user?.count}</Text>
 					</div>
 				{/each}
