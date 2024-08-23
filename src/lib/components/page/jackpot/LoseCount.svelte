c20d1b99ccf3d4c32754ec2ccc9395ae0a27a350
DavidYong <yongcheeho@gmail.com>
2024-08-23 20:45:40 +0800

    chores: added log in check for jet and lose count

--- src/lib/components/page/jackpot/LoseCount.svelte
+++ src/lib/components/page/jackpot/LoseCount.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { t } from '$lib/i18n';
 	import Text from '$lib/components/ui/text/text.svelte';
+    import { storeUserInfo } from "$lib/stores/storeUser";
+    import { zeroAddress } from "viem";
     
 	export let jackpotPoolLoseCount: number = 0;
     export let classes = ''
@@ -10,7 +12,8 @@
 <div id="total game lose" class="flex flex-col gap-2 bg-black/25 rounded-2xl px-5 py-3 w-full  items-center {classes}">
     <Text tag="h1" size="lg" class="">{$t('jackpot.total_lose_times')}:</Text>
     <div class="flex justify-center items-center gap-1">
-        {#if jackpotPoolLoseCount === 0}
+
+        {#if jackpotPoolLoseCount === 0 || $storeUserInfo.web3_address === zeroAddress}
         <Text size="4xl" class="leading-none"> - </Text>
         {:else}
         <Text tag="h1" size="default" class=" text-[20px] font-bold text-[#DF61FF] translate-y-[-5%]"
